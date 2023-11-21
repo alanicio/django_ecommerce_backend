aa426349d7cb88a6542b3c38633e4302e24a9e02
alanicio <alanicio98@outlook.com>
2023-11-20 22:18:34 -0600

    modelViewSet added in Item

--- item/views.py
+++ item/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Item
+from .serializer import ItemSerializer
 
-# Create your views here.
+
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+    """ def retrieve(self, request, *args, **kwargs):
+        #todo anything
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data) """
