6796c430ab693b5c66a9a52b8aded080231bcb47
alanicio <alanicio98@outlook.com>
2023-11-20 19:46:24 -0600

    added models to admin

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.DiscountType)
+admin.site.register(models.Discount)
+admin.site.register(models.Order)
+admin.site.register(models.ItemOrder)
